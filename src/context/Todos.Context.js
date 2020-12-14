be9c3eeb61ddfdd4721dc72e7efbe3a4e3e86658
Ashish Sangale <aashishsangale07@gmail.com>
2020-12-14 13:21:25 +0530

    consuming localstorage reducer hook

--- src/context/Todos.Context.js
+++ src/context/Todos.Context.js
@@ -1,4 +1,5 @@
-import React,{useReducer, createContext} from 'react';
+import React,{ createContext } from 'react';
+import { useLocalStorageReducer } from '../hooks/useLocalStorageReducer';
 import todoReducer from '../reducers/Todos.reducer';
 const defaultTodo = [
     {id: 1, task: 'task1', completed: false},
@@ -8,7 +9,7 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props){
-    const [todos, dispatch] = useReducer(todoReducer, defaultTodo)
+    const [todos, dispatch] = useLocalStorageReducer('todos', defaultTodo, todoReducer)
     return(
         <TodosContext.Provider value={todos}>
             <DispatchContext.Provider value={dispatch}>
